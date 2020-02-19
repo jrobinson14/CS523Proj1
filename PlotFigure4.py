72465db7f9c8fa5575eb2adbc5ed427b66e87d6f
Jarett Jones <JCH_jones@ymail.com>
2020-02-19 01:41:45 -0700

    Figured out how to do a list comprehension and added in the proper code to read in a set of bit data from file. Each data point can either be on a new line or seperated by spaces. New line is preffered as its tested and works

--- PlotFigure4.py
+++ PlotFigure4.py
@@ -5,28 +5,27 @@ import pylab
 
 def generate_plot(rate):
 
-    p = 0 # reference epsilon value
+    p = 0  # reference epsilon value
 
-    #there are 3 values per epsilon
-    top_down_list = [][3]
-    bottom_up_list = [][3]
-    middle_list = [][3]
+    # there are 10 values per value of epsilon
+    top_down_list = [[]]
+    top_down_list.pop(0)
+    bottom_up_list = [[]]
+    bottom_up_list.pop(0)
+    middle_list = [[]]
+    middle_list.pop(0)
 
     while p <= 1:
         top_down_file = open("ep" + str(p) + "\\TopDownEntropy.txt", "r")
-        bottom_up_file = open("ep" + str(p) + "\\BottomUpEntropy.txt", "r")
+        # bottom_up_file = open("ep" + str(p) + "\\BottomUpEntropy.txt", "r")
 
-        top_down_list.append = top_down_file.read()
-        bottom_up_list.append = bottom_up_file.read()
-        middle_list.append(top_down_list[p]-bottom_up_list[p])
+        top_down_list.append([])
+        top_down_list[len(top_down_list)-1].append(p)  # make the current epsilon value the first value of the list
+        top_down_list[len(top_down_list)-1] = [[float(x) for x in line.split()] for line in top_down_file]
+        # bottom_up_list.append = bottom_up_file.read()
+        # middle_list.append(top_down_list[p]-bottom_up_list[p])
 
         p += rate
-
-
-
-
-
-
 
 
 generate_plot(0.05)
